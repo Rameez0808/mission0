0c8f12a7db407133422c6f1fc5ea9d81eebd4010
Karan Singh <iampatwari@gmail.com>
2022-11-15 11:08:47 +1300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import "./App.css";
+import TopNavBar from "./Components/TopNavBar/TopNavBar";
+
+function App() {
+  return (
+    <Fragment>
+      <TopNavBar />
+    </Fragment>
+  );
+}
+
+export default App;
