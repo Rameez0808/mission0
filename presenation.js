8a59eb21d091457eb07cf479c8193826c0dd3d99
Karan Singh <iampatwari@gmail.com>
2022-11-17 22:25:30 +1300

    UPD: Added a presentation file

---
+++ presenation.js
@@ -0,0 +1,12 @@
+// 0. React and Tailwind CSS
+// 1. Explain components
+// 2. Features -> hamburger animation,
+//    search feature, onscroll color
+//    change, responsive
+// 3. Feedback -> feedback file
+// 4. Some more Features ->
+// -> Nav-Link disappears feature
+// -> Hamburger to cross button animation
+// -> Search No result find
+
+// https://imkaransandhu.github.io/mission0/
